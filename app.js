2d8a941c38eae802ee4aea943a8ed22e6b1e463a
Mathias Nikkel <mathiasnikkel@gmail.com>
2019-11-09 12:45:53 -0300

    commit on catalina

--- app.js
+++ app.js
@@ -1,49 +1,58 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// importa a funcionalidade de session do express
+const session = require("express-session");
+// conecta a session com o mongo
+const mongoDBstore = require("connect-mongodb-session")(session);
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const MONGO_DB_URI =
+  "mongodb+srv://mnikkel-mongo:0204@cluster0-ob6d3.mongodb.net/shop";
 
 const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+const store = new mongoDBstore({
+  uri: MONGO_DB_URI,
+  collection: "sessions"
 });
 
-app.use('/admin', adminRoutes);
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+// consfigura a session, evitando que seja salvo sem necessidade (por isso dos false)
+app.use(
+  session({
+    secret: "alguma string longa",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
+  .connect(MONGO_DB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: "Max",
+          email: "max@test.com",
           cart: {
             items: []
           }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,25 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+  // recupera o valor do cookie
+  const isLoggedIn = req.get("Cookie").split("=")[1];
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-  };
+  // método que salva um cookie para o usuário logado
+  // res.setHeader("Set-Cookie", "loggedIn=true");
+
+  User.findById("5dc3613fec95612d546e2182")
+    .then(user => {
+      console.log(user);
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch(err => console.log(err));
+};
